190ca2ec1db705a7ffe3a2b61d4be7f763f44f23
fansg666 <2680315178@qq.com>
2024-05-28 20:07:28 +0800

    app.Before 内初始化 logrus 的日志配置。

--- main.go
+++ main.go
@@ -8,58 +8,57 @@ import (
 	"github.com/urfave/cli"
 )
 
-// urfaveCli cli 包简单使用，具体可以参考官方文档
+const usage = `mydocker is a simple container runtime implementation.
+			   The purpose of this project is to learn how docker works and how to write a docker by ourselves
+			   Enjoy it, just for fun.`
+
 func main() {
 	app := cli.NewApp()
+	app.Name = "mydocker"
+	app.Usage = usage
 
-	// 指定全局参数
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:  "lang, l",
-			Value: "english",
-			Usage: "Language for the greeting",
-		},
-		cli.StringFlag{
-			Name:  "config, c",
-			Usage: "Load configuration from `FILE`",
-		},
-	}
-	// 指定支持的命令列表
 	app.Commands = []cli.Command{
-		{
-			Name:    "complete",
-			Aliases: []string{"c"},
-			Usage:   "complete a task on the list",
-			Action: func(c *cli.Context) error {
-				log.Println("run command complete")
-				for i, v := range c.Args() {
-					log.Printf("args i:%v v:%v\n", i, v)
-				}
-				return nil
-			},
-		},
-		{
-			Name:    "add",
-			Aliases: []string{"a"},
-			// 每个命令下面还可以指定自己的参数
-			Flags: []cli.Flag{cli.Int64Flag{
-				Name:  "priority",
-				Value: 1,
-				Usage: "priority for the task",
-			}},
-			Usage: "add a task to the list",
-			Action: func(c *cli.Context) error {
-				log.Println("run command add")
-				for i, v := range c.Args() {
-					log.Printf("args i:%v v:%v\n", i, v)
-				}
-				return nil
-			},
-		},
+		initCommand,
+		runCommand,
 	}
 
-	err := app.Run(os.Args)
-	if err != nil {
+	app.Before = func(context *cli.Context) error {
+		// Log as JSON instead of the default ASCII formatter.
+		log.SetFormatter(&log.JSONFormatter{})
+
+		log.SetOutput(os.Stdout)
+		return nil
+	}
+
+	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
 }
+
+//func main() {
+//	app := cli.NewApp()
+//	app.Name = "ls"
+//	app.Usage = "List directory contents"
+//	app.Action = func(c *cli.Context) error {
+//		dir, err := os.Open(".") // 默认打开当前目录
+//		if err != nil {
+//			return err
+//		}
+//		defer dir.Close()
+//
+//		fileInfos, err := dir.Readdir(-1) // 列出所有文件
+//		if err != nil {
+//			return err
+//		}
+//
+//		for _, fi := range fileInfos {
+//			fmt.Println(fi.Name())
+//		}
+//		return nil
+//	}
+//
+//	err := app.Run(os.Args)
+//	if err != nil {
+//		fmt.Println(err)
+//	}
+//}
